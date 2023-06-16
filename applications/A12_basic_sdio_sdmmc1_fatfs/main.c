f2e15c0d5902a69b6c3f680e6bdaf82f4a4fd493
Flicker372 <flicker372@outlook.com>
2023-06-16 13:45:51 +0800

    Main: Add Copyright

--- applications/A12_basic_sdio_sdmmc1_fatfs/main.c
+++ applications/A12_basic_sdio_sdmmc1_fatfs/main.c
@@ -1,3 +1,16 @@
+  
+/*
+******************************************************************************
+* <h2><center>&copy; Copyright (c) 2022 STMicroelectronics.
+* All rights reserved.</center></h2>
+*
+* This software component is licensed by ST under BSD 3-Clause license,
+* the "License"; You may not use this file except in compliance with the
+* License. You may obtain a copy of the License at:
+*                        opensource.org/licenses/BSD-3-Clause
+*
+******************************************************************************
+*/
 // 头文件
 #include "main.h"
 #include "dma.h"
@@ -214,3 +227,4 @@ void assert_failed(uint8_t *file, uint32_t line)
 
 }
 #endif
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
