426e0c8dc671fa1cba7e5d61e5b851654f49d4b8
Flicker372 <flicker372@outlook.com>
2023-06-16 13:41:39 +0800

    E-albums: Init

--- applications/A12_basic_sdio_sdmmc1_fatfs/main.c
+++ applications/A12_basic_sdio_sdmmc1_fatfs/main.c
@@ -1,217 +1,140 @@
-/* USER CODE BEGIN Header */
-/**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; Copyright (c) 2022 STMicroelectronics.
-  * All rights reserved.</center></h2>
-  *
-  * This software component is licensed by ST under BSD 3-Clause license,
-  * the "License"; You may not use this file except in compliance with the
-  * License. You may obtain a copy of the License at:
-  *                        opensource.org/licenses/BSD-3-Clause
-  *
-  ******************************************************************************
-  */
-/* USER CODE END Header */
-/* Includes ------------------------------------------------------------------*/
+// Â§¥Êñá‰ª∂
 #include "main.h"
-#include "adc.h"
-#include "dac.h"
 #include "dma.h"
 #include "i2c.h"
 #include "usart.h"
 #include "quadspi.h"
 #include "sdmmc.h"
 #include "spi.h"
-#include "tim.h"
 #include "gpio.h"
-
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
-#include "stdio.h"
 #include "fatfs.h"
-/* USER CODE END Includes */
+#include "lcd.h"
+#include "stdio.h"
+#include "string.h"
 
-/* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
+// ÂÆèÂÆö‰πâ
+#define pic_size 115200 // ÂõæÁâáÂ§ßÂ∞èÔºåÂ°´Âçï‰∏™binÊñá‰ª∂Â§ßÂ∞èÔºåÂ≠òÂÇ®16ËøõÂà∂È¢úËâ≤‰ø°ÊÅØÔºåÂú®Â∞èÁÜäÊ¥æ‰∏äËøáÂ§ßÁºñËØëÊó∂‰ºöÊä•ÂÜÖÂ≠òÊ∫¢Âá∫
+#define filename "pic.bin" // Êñá‰ª∂ÂêçÁß∞
 
-/* USER CODE END PTD */
+// Â£∞ÊòéÂáΩÊï∞
+void SystemClock_Config(void); // Á≥ªÁªüÊó∂Èíü
+void PeriphCommonClock_Config(void); // Â§ñÈÉ®Êó∂Èíü
+void MountSD(void); // ÊåÇËΩΩSDÂç°
+void Show_Pic_Auto(TCHAR const*); // LCDÊòæÁ§∫Êï∞ÁªÑ‰∏≠ÁöÑÂõæÁâá
+void Show_Pic_Manual(TCHAR const*); // LCDÊòæÁ§∫Êï∞ÁªÑ‰∏≠ÁöÑÂõæÁâá
 
-/* Private define ------------------------------------------------------------*/
-/* USER CODE BEGIN PD */
+// Â£∞Êòé
+int stats = 0; // Áä∂ÊÄÅ
+int file_size = 0; // Â£∞ÊòéÂπ∂ÂàùÂßãÂåñÊñá‰ª∂Â§ßÂ∞èÂèòÈáè
+int offset = 0; // Â£∞ÊòéÂπ∂ÂàùÂßãÂåñÂÅèÁßªÈáèÂèòÈáèÔºå‰ªéÂ§¥ÂºÄÂßã
+int jet = 1;
+FATFS fs;
+FIL file;
+UINT br;
 
-/* USER CODE END PD */
-
-/* Private macro -------------------------------------------------------------*/
-/* USER CODE BEGIN PM */
-
-/* USER CODE END PM */
-
-/* Private variables ---------------------------------------------------------*/
-
-/* USER CODE BEGIN PV */
-void SD_FatFs_OpenReadFileTest(TCHAR const* OpenOrCreatTXTFileName,BYTE* WriteData,uint8_t Size);
-uint8_t  f_GetTotal_Free(uint8_t *drv,uint32_t *total,uint32_t *free);
-FATFS fs;	// FatFsŒƒº˛œµÕ≥∂‘œÛ 
-FIL file;	// Œƒº˛∂‘œÛ 
-FRESULT f_res;     //Œƒº˛≤Ÿ◊˜Ω·π˚  
-UINT fnum;       // Œƒº˛≥…π¶∂¡–¥ ˝¡ø  
-BYTE ReadBuffer[1024]={0};        // ∂¡ª∫≥Â«¯ 
-TCHAR TXTFileName[]="–°–‹≈…ø™∑¢∞ÂSDø®FATFSŒƒº˛œµÕ≥≤‚ ‘.txt";
-BYTE WriteDataToTXT[]= "ª∂”≠ π”√ŒÔ¡™Õ¯–°–‹≈…ø™∑¢∞Â,±æΩÃ≥Ã «SDø®FATFSŒƒº˛œµÕ≥ µ—È";//–¥ª∫≥Â«¯
-/* USER CODE END PV */
-
-/* Private function prototypes -----------------------------------------------*/
-void SystemClock_Config(void);
-void PeriphCommonClock_Config(void);
-/* USER CODE BEGIN PFP */
-
-/* USER CODE END PFP */
-
-/* Private user code ---------------------------------------------------------*/
-/* USER CODE BEGIN 0 */
-uint8_t f_GetTotal_Free(uint8_t *drv, uint32_t *total, uint32_t *free)
-{
-    FATFS *fs1;
-    uint8_t res;
-    DWORD fre_clust = 0, fre_sect = 0, tot_sect = 0;
-    f_res = f_mount(fs1, (TCHAR const*)SDPath, 1);
-    res = f_getfree((const TCHAR*)drv, &fre_clust, &fs1);//µ√µΩ¥≈≈Ã–≈œ¢º∞ø’œ–¥ÿ ˝¡ø
-    if (res == 0) {
-        tot_sect = (fs1->n_fatent - 2) * fs1->csize;    //µ√µΩ◊‹…»«¯ ˝
-        fre_sect = fre_clust * fs1->csize;              //µ√µΩø’œ–…»«¯ ˝
-#if _MAX_SS!=512                                        //…»«¯¥Û–°≤ª «512◊÷Ω⁄,‘Ú◊™ªªŒ™512◊÷Ω⁄
-        tot_sect *= fs1->ssize / 512;
-        fre_sect *= fs1->ssize / 512;
-#endif      
-        *total = tot_sect >> 1;     //µ•ŒªŒ™KB
-        *free = fre_sect >> 1;      //µ•ŒªŒ™KB 
-    }
-    f_res = f_mount(NULL, (TCHAR const*)SDPath, 1);	//≤ª‘Ÿ π”√£¨»°œ˚π“‘ÿ 
-    return res;
-}
-static void SD_FatFs_OpenReadFileTest(TCHAR const* OpenOrCreatTXTFileName,BYTE* WriteData,uint8_t Size)
-{
- if(retSD == 0)
-  {
-   f_res = f_mount(&fs,(TCHAR const*)SDPath,1);//π“‘ÿ ±ª·∂‘SDø®≥ı ºªØ
-   f_res = f_open(&file, OpenOrCreatTXTFileName,FA_CREATE_ALWAYS | FA_WRITE );
-    if ( f_res == FR_OK )
-    {
-      f_res=f_write(&file,WriteData,Size,&fnum);   
-      if(f_res==FR_OK) 
-      printf("Write dat to SD file %s success£°\r\n",OpenOrCreatTXTFileName);				
-      f_close(&file); // ≤ª‘Ÿ∂¡–¥£¨πÿ±’Œƒº˛ 
-    }
- else printf("Open or creat SD file %s fail£°\r\n",OpenOrCreatTXTFileName);					      
-    f_res = f_open(&file, OpenOrCreatTXTFileName, FA_OPEN_EXISTING | FA_READ); 	 
-    if(f_res == FR_OK)
-    {
-        f_res = f_read(&file, ReadBuffer, sizeof(ReadBuffer), &fnum); 
-        if(f_res==FR_OK)  
-        printf("Read data from SD file %s£∫\r\n%s \r\n",OpenOrCreatTXTFileName, ReadBuffer);	
-         f_close(&file); // ≤ª‘Ÿ∂¡–¥£¨πÿ±’Œƒº˛ 
-    } 
-      else printf("Open or creat SD file %s fail£°\r\n",OpenOrCreatTXTFileName);	
-    f_res = f_mount(NULL,(TCHAR const*)SDPath,1);	// ≤ª‘Ÿ π”√£¨»°œ˚π“‘ÿ 
-    FATFS_UnLinkDriver(SDPath);// ◊¢œ˙“ª∏ˆFatFs…Ë±∏
-   }
-}
-/* USER CODE END 0 */
-
-/**
-  * @brief  The application entry point.
-  * @retval int
-  */
+// ‰∏ªÂáΩÊï∞
 int main(void)
 {
-  /* USER CODE BEGIN 1 */
-  uint32_t Total=0;//∂¡»°SDø®◊‹»›¡ø
-  uint32_t Free=0;//∂¡»°SDø® £”‡»›¡ø
-  /* USER CODE END 1 */
-
-  /* MCU Configuration--------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  // Á°¨‰ª∂ÊäΩË±°Â±ÇÂàùÂßãÂåñ
   HAL_Init();
 
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
+  // ÈÖçÁΩÆÊó∂Èíü
   SystemClock_Config();
-
-  /* Configure the peripherals common clocks */
   PeriphCommonClock_Config();
 
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
+  // ÂàùÂßãÂåñÂ§ñÂõ¥ËÆæÂ§á
   MX_GPIO_Init();
   MX_DMA_Init();
   MX_LPUART1_UART_Init();
+  MX_USART1_UART_Init();
   MX_USART2_UART_Init();
   MX_USART3_UART_Init();
   MX_QUADSPI_Init();
   MX_SDMMC1_SD_Init();
   MX_I2C1_Init();
   MX_I2C3_Init();
-  MX_USART1_UART_Init();
   MX_SPI1_Init();
   MX_SPI2_Init();
   MX_SPI3_Init();
-  MX_ADC1_Init();
-  MX_DAC1_Init();
-  MX_TIM16_Init();
 	MX_FATFS_Init();
-  /* USER CODE BEGIN 2 */
-  printf("***********Test fatfs of SD card**********\r\n");	
-  f_GetTotal_Free((uint8_t*)"0:",&Total,&Free);//ªÒ»°SDø®◊‹»›¡ø∫Õ £”‡»›¡ø
-  printf("Total:%dKB=%0.2fGB,free:%dKB=%0.2fGB\r\n", Total, (float)Total/1024/1024, Free,(float)Free/1024/1024);	
-  SD_FatFs_OpenReadFileTest(TXTFileName,WriteDataToTXT,sizeof(WriteDataToTXT));//¥Úø™Œƒº˛≤¢–¥»Î ˝æ›∫Õ∂¡»° ˝æ›
-  /* USER CODE END 2 */
+  LCD_Init(); // LCDÂ±èÂàùÂßãÂåñ
 
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
+  MountSD(); // ÊåÇËΩΩSDÂç°
+
   while (1)
   {
-    /* USER CODE END WHILE */
-    /* USER CODE BEGIN 3 */
-    HAL_GPIO_TogglePin(LED_GPIO_Port,LED_Pin);
-    HAL_Delay(1000);
+    Show_Pic_Auto(filename); // Êñá‰ª∂ÊîæÂú®SDÂç°Ê†πÁõÆÂΩïÔºå‰º†ÂÖ•binÊñá‰ª∂ÂêçÁß∞
   }
-  /* USER CODE END 3 */
 }
 
-/**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+void MountSD()
+{
+  f_mount(&fs, (TCHAR const*)SDPath, 1); // ÊåÇËΩΩSDÂç°
+}
+
+// ÈÄêÂ∏ßÊòæÁ§∫Êï∞ÁªÑ‰∏≠ÁöÑÂõæÁâá
+void Show_Pic_Auto(TCHAR const* OpenOrCreatTXTFileName)
+{
+  uint16_t i;
+  uint8_t data[1200]; //Â≠òÂÇ®‰∫îË°åÂÉèÁ¥†ÁÇπ240 ÁöÑ 16ËøõÂà∂È¢úËâ≤‰ø°ÊÅØ 240*5=1200
+
+  f_open(&file, OpenOrCreatTXTFileName, FA_READ);
+  file_size = f_size(&file); // ËÆ°ÁÆóÂêàÂπ∂ÂêébinÊñá‰ª∂ÁöÑÂ§ßÂ∞è
+
+  while (stats == 1 || stats == 0)
+  {
+    if (stats == 0)
+    {
+      HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_SET); // ‰∫Æ
+      for(i = 0; i < 96; i++)
+      {
+        f_read(&file, data, 1200, &br);//xÊñπÂêë240‰∏™ÂÉèÁ¥†ÁÇπÔºåÊØè‰∏™ÂÉèÁ¥†ÁÇπ 2 ‰∏™
+        SPI2_WriteByte(data, 1200);//ÊòæÁ§∫ 5 Ë°åÂÉèÁ¥†ÁÇπ
+      }
+      HAL_Delay(1000);
+      offset += pic_size; // ÂÅèÁßªÈáèÔºå‰∏ã‰∏ÄÂº†ÂõæÁâá
+      if (offset >= file_size)
+      {
+        offset = 0;
+      }
+      f_lseek(&file, offset); // ÁßªÂä®Êñá‰ª∂ÊµÅÁöÑËØªÂÜô‰ΩçÁΩÆ
+      HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_RESET); // ÁÅ≠
+      HAL_Delay(1000);
+    }
+    else if (stats == 1)
+    {
+      for(i = 0; i < 96; i++)
+      {
+        f_read(&file, data, 1200, &br);//xÊñπÂêë240‰∏™ÂÉèÁ¥†ÁÇπÔºåÊØè‰∏™ÂÉèÁ¥†ÁÇπ 2 ‰∏™
+        SPI2_WriteByte(data, 1200);//ÊòæÁ§∫ 5 Ë°åÂÉèÁ¥†ÁÇπ
+      }
+      offset = 0;
+      offset += pic_size * jet; // ÂÅèÁßªÈáèÔºå‰∏ã‰∏ÄÂº†ÂõæÁâá
+      if (offset >= file_size)
+      {
+        offset = 0;
+        jet = 0;
+      }
+      f_lseek(&file, offset); // ÁßªÂä®Êñá‰ª∂ÊµÅÁöÑËØªÂÜô‰ΩçÁΩÆ
+    }
+  }
+
+  f_close(&file);
+}
+
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-  /** Configure the main internal regulator output voltage
-  */
   if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1) != HAL_OK)
   {
     Error_Handler();
   }
-  /** Configure LSE Drive Capability
-  */
+
   HAL_PWR_EnableBkUpAccess();
   __HAL_RCC_LSEDRIVE_CONFIG(RCC_LSEDRIVE_LOW);
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
+
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI|RCC_OSCILLATORTYPE_LSE
                               |RCC_OSCILLATORTYPE_MSI;
   RCC_OscInitStruct.LSEState = RCC_LSE_ON;
@@ -231,8 +154,6 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
@@ -244,21 +165,14 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
-  /** Enable MSI Auto calibration
-  */
+
   HAL_RCCEx_EnableMSIPLLMode();
 }
 
-/**
-  * @brief Peripherals Common Clock Configuration
-  * @retval None
-  */
 void PeriphCommonClock_Config(void)
 {
   RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
 
-  /** Initializes the peripherals clock
-  */
   PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_SDMMC1|RCC_PERIPHCLK_ADC;
   PeriphClkInit.AdcClockSelection = RCC_ADCCLKSOURCE_PLLSAI1;
   PeriphClkInit.Sdmmc1ClockSelection = RCC_SDMMC1CLKSOURCE_PLLSAI1;
@@ -275,40 +189,28 @@ void PeriphCommonClock_Config(void)
   }
 }
 
-/* USER CODE BEGIN 4 */
+// Â§ñÈÉ®‰∏≠Êñ≠
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) // Â§ñÈÉ®‰∏≠Êñ≠ÂõûË∞ÉÂáΩÊï∞
+ {
+  if (GPIO_Pin == KEY1_Pin) // Ê£ÄÊµãÊòØÂê¶‰∏∫Â§ñÈÉ®‰∏≠Êñ≠2
+	{
+    stats = 1;
+    jet++;
+	}
+	if (GPIO_Pin == KEY2_Pin) // Ê£ÄÊµãÊòØÂê¶‰∏∫Â§ñÈÉ®‰∏≠Êñ≠3
+	{
+    stats = 2;
+	}
+ }
 
-/* USER CODE END 4 */
-
-/**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
 void Error_Handler(void)
 {
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
-  while (1)
-  {
-  }
-  /* USER CODE END Error_Handler_Debug */
+
 }
 
 #ifdef  USE_FULL_ASSERT
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
 void assert_failed(uint8_t *file, uint32_t line)
 {
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
-}
-#endif /* USE_FULL_ASSERT */
 
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
+}
+#endif
